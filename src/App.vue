4a0c65333562be4a9a9a457971e494c10666943e
murtazaaylak <murtazaaylak@gmail.com>
2021-10-20 23:42:20 +0300

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="nav-header">
+    <nav type="dark" class="px-5">
+      <div href="#" title="Don't do it">
+        <img src="../assets/mike-logo.jpeg" alt="mike-logo" />
+      </div>
+      <div class="cart-wrapper">
+        <div class="cart-icon-container">
+          <b-icon icon="bag" class="cart-icon" />
+
+          <div class="product-count">{{ itemCount }}0</div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    itemCount: Number,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav-header {
+  width: 100vw;
+  border-bottom: 1px solid #ebebeb;
+  padding: 10px;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+img {
+  width: 70px;
+  border-radius: 3px;
+}
+
+.cart-wrapper {
+  background-color: transparent;
+  padding: 10px;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100%;
+}
+
+.cart-wrapper:hover {
+  background-color: #ebebeb;
+}
+.cart-icon-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.product-count {
+  font-size: 11px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 3px;
+  color: #000;
+  font-weight: 600;
+  width: 24px;
+  height: 24px;
+}
+.cart-icon {
+  color: #000;
+  font-size: 24px;
+}
+</style>
